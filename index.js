70e5e7d0a0a759185695f1cbfff18ad88ab93b15
shriyaborkar358 <shriyaborkar71@gmail.com>
2024-07-02 00:47:24 +0530

    changes added

--- index.js
+++ index.js
@@ -189,6 +189,13 @@ app.delete("/book/:id", (req, res)=>{
     })
 })
 
+app.use("*", (req, res)=>{
+
+    res.send(`<div>
+        <h1 style="text-align: center;">404 not found </h1>
+        </div>`)
+})
+
 const PORT = process.env.PORT
 
 app.listen(PORT, ()=>{
